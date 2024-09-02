74ef53b99aa154c76b728f552e7e3cd3754ef1d3
t-prince-king <princetanga42@gmail.com>
2024-09-02 12:10:52 -0700

    js info

---
+++ yui.js
@@ -0,0 +1,15 @@
+let width = 4;
+let height = 5;
+
+console.log(`width: ${width} cm`);
+console.log(`height: ${height} mm`);
+
+    // area = width = heigth
+    let area = width * height
+    console.log(`area: ${area} cm2`);
+
+    // perimeter = 2 * (width + heigth)
+
+    let perimeter = 2 * (width + height);
+    console.log(`perimeter: ${perimeter} cm`);
+
